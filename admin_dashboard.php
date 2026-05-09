e81e2d4a07f9f4bb505fd72ad002f7d4cc31027c
Blessings Emulyn <blessingsemulyn@gmail.com>
2026-05-09 11:02:35 +0200

    Added error reporting to dashboard files

--- admin_dashboard.php
+++ admin_dashboard.php
@@ -1,4 +1,7 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 require_once 'check_remember_me.php';
 require_once 'config.php';
 session_start();
